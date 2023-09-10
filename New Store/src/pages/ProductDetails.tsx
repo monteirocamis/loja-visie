5aa64c9ed00183a89b37f9e76b222f9f07412297
moca xx <mocaxx@mocas-pro.local>
2023-09-10 01:14:52 -0300

    :sparkles: prices with discount and favorite

--- New Store/src/pages/ProductDetails.tsx
+++ New Store/src/pages/ProductDetails.tsx
@@ -1,3 +1,27 @@
+import { Carousel , Row, Col, Container } from "react-bootstrap";
+import { ProductItem } from "../components/ProductItem"
+import storeItems from "../data/items.json"
+import { formatCurrency } from "../utilities/formatCurrency"
+import { CartItem } from "../components/CartItem"
+import { useShoppingCart } from "../context/ShoppingCartContext"
+
 export function ProductDetails() {
-  return <h1> produto</h1>
+  const { closeCart, cartItems } = useShoppingCart()
+
+  return (
+<>
+  <h1> produto</h1>
+  {cartItems.map(item => (
+    <CartItem key={item.id} {...item} />
+    ))}
+     
+            {formatCurrency(
+              cartItems.reduce((total, cartItem) => {
+                const item = storeItems.find(i => i.id === cartItem.id)
+                return total + (item?.price || 0) * cartItem.quantity
+              }, 0)
+              )}
+ </>
+
+  )
 }
---
+++ New Store/src/styles.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+// type ContainerProps = {
+//     haveDiscount: boolean;
+// }
+
+// const ContainerStyled = styled.div`
+//   color: #CCC;
+// /* text-decoration: ${haveDiscount ? 'line-through' : 'initial'} */
+//              `
+
+
+
+export const Lined = styled.div`
+  text-decoration:line-through;
+  color: #454b50;
+`
