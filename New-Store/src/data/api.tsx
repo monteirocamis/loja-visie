aefd5a133e3f4b9d785fedacaf213b4008f10e72
moca xx <mocaxx@mocas-pro.local>
2023-09-11 23:10:39 -0300

    changed folders

--- New-Store/src/data/api.tsx
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-export const api = axios.create({
-    baseURL: 'https://dummyjson.com',
-    headers:{  'Content-Type': 'application/json'    },
-    timeout: 3000
-})
--- New-Store/src/types/price.tsx
+++
@@ -1,5 +0,0 @@
-export type Item = {
-    id: number;
-    haveDiscount: boolean; //done
-  
-}
